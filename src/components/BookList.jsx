3d8e85a4b8ec622a13325b5e600a6961d22fa7c6
Parvathi Kumar <parvathikumar92@gmail.com>
2025-11-25 11:54:44 -0600

    styled booklist

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -13,22 +13,26 @@ const BookList = () => {
 
   return (
     <div>
-      <ul className="list rounded-box shadow-md">
-        <li className="p-4 pb-2 text-md font-semibold tracking-wide">Recently Read: </li>
+    <div className="p-4 pb-2 text-md font-semibold tracking-wide">Recently Read: </div>
+    <div className="flex gap-4 overflow-x-auto">
         {books.map((userBook, index) => {
           const book = userBook.book;
-          console.log(book);
           return (
-            <li key={index} id={index} className="list-row">
-              <div><img className="w-[50px] rounded-box" src={book.image.url}/></div>
-              <div>
-                <div className="font-semibold uppercase text-md">{book.title}</div>
-                <div className="text-xs">{book.contributions[0].author.name}</div>
+            <div key={index} id={index} className="card !p-1 w-48 shadow-sm">
+              <figure className="px-2 pt-2">
+                <img
+                  src={book.image.url}
+                  alt={book.title}
+                  className="rounded-md max-h-[175px]" />
+              </figure>
+              <div className="card-body !p-2 text-center gap-0">
+                <h5 className="card-title self-center text-sm">{book.title}</h5>
+                <p className="text-xs uppercase">{book.contributions[0].author.name}</p>
               </div>
-            </li>
+            </div>
             )}
           )}
-      </ul>
+    </div>
     </div>
   )
 };
