6e891c41408ef9709d5255cc19973474fa304d07
Parvathi Kumar <parvathikumar92@gmail.com>
2025-12-02 15:43:47 -0600

    netlify form handling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,31 +1,51 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState } from 'react'; 
 
+  const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
 
 export default function Contact() {
     const [visitorName, setVisitorName] = useState(''); 
     const [visitorEmail, setVisitorEmail] = useState(''); 
     const [visitorMessage, setVisitorMessage] = useState(''); 
+    const [submitMessage, setSubmitMessage] = useState(false); 
 
     const submitContactForm = (e) => {
-        e.preventDefault();
-        console.log(e);
+         e.preventDefault();
+        fetch("/", {
+            method: "POST", 
+            headers: { "Content-Type": "application/x-www-form-urlencoded" }, 
+            body: encode({ "form-name": "contact-form", "Name": visitorName, "Email": visitorEmail, "Message": visitorMessage})
+        })
+            .then(() => setSubmitMessage(true))
+            .catch(error => alert(error)); 
+            setVisitorEmail(''); 
+            setVisitorName(''); 
+            setVisitorMessage(''); 
     }
     return(
         <div className="text-left m-10">
             <h2>Contact</h2>
-            <div id="contact-div" class="mockup-code w-1/2 my-10 mx-auto bg-[var(--mustard)]">
-                <pre data-prefix="$"><code>// want to collaborate?</code></pre>
-                <pre data-prefix="$"><code>// connect with me!</code></pre>
-                <form onSubmit={(e) => submitContactForm(e)}>
-                    <pre data-prefix=">" className="text-[var(--brown)]"><code>const name = </code>
+            <div id="contact-div" class="mockup-code w-1/2 my-10 mx-auto bg-[var(--olive)]">
+                <pre data-prefix="$" className="text-[var(--cream)]"><code>// want to collaborate?</code></pre>
+                <pre data-prefix="$" className="text-[var(--cream)]"><code>// connect with me!</code></pre>
+                <br/>
+                <form name="contact-form" data-netlify="true" onSubmit={(e) => submitContactForm(e)}>
+                    <pre data-prefix=">" className="text-[var(--cream)]"><code>your name:  </code>
                      <input value={visitorName} onChange={(e) => setVisitorName(e.target.value)} type="text" autofocus ></input></pre>
-                     <pre data-prefix=">" className="text-[var(--brown)]"><code>const email = </code>
+                     <pre data-prefix=">" className="text-[var(--cream)]"><code>your email:  </code>
                     <input type="email" value={visitorEmail} onChange={(e) => setVisitorEmail(e.target.value)}></input></pre>
-                    <pre data-prefix=">" className="text-[var(--brown)]"><code>const message = </code>
+                    <pre data-prefix=">" className="text-[var(--cream)]"><code>message:  </code>
                     <input type="text"value={visitorMessage} onChange={(e) => setVisitorMessage(e.target.value)}></input></pre>
                     <pre className="flex justify-between my-5"><code><button id="contact-button" type="submit">sendMessage()</button></code></pre>
-                 </form>
+                {submitMessage && 
+                    <pre data-prefix="$" className="bg-success"><code>SUCCESS: message received </code></pre>
+                }
+            </form>
             </div>
         </div>
     )
