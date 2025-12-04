31152994c592704726704725c492ccaa6183821f
Parvathi Kumar <parvathikumar92@gmail.com>
2025-12-04 09:33:26 -0600

    final form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,8 +14,8 @@ export default function Contact() {
     const [visitorMessage, setVisitorMessage] = useState(''); 
     const [submitMessage, setSubmitMessage] = useState(false); 
 
-    // const submitContactForm = (e) => {
-    //      e.preventDefault();
+     const handleClick = (e) => {
+         e.preventDefault();
     //     fetch("/", {
     //         method: "POST", 
     //         headers: { "Content-Type": "application/x-www-form-urlencoded" }, 
@@ -23,10 +23,11 @@ export default function Contact() {
     //     })
     //         .then(() => setSubmitMessage(true))
     //         .catch(error => alert(error)); 
-    //         setVisitorEmail(''); 
-    //         setVisitorName(''); 
-    //         setVisitorMessage(''); 
-    // }
+            setSubmitMessage(true);
+            setVisitorEmail(''); 
+            setVisitorName(''); 
+            setVisitorMessage(''); 
+    }
     return(
         <div className="text-left m-10">
             <h2>Contact</h2>
@@ -34,14 +35,15 @@ export default function Contact() {
                 <pre data-prefix="$" className="text-[var(--cream)]"><code>// want to collaborate?</code></pre>
                 <pre data-prefix="$" className="text-[var(--cream)]"><code>// connect with me!</code></pre>
                 <br/>
-                <form name="contact-form" method="POST" netlify>
+                <form name="contact-form" method="POST" data-netlify="true">
+                    <input type="hidden" name="form-name" value="contact-form" />
                     <pre data-prefix=">" className="text-[var(--cream)]"><code>your name:  </code>
                      <input name="name" value={visitorName} onChange={(e) => setVisitorName(e.target.value)} type="text" autofocus ></input></pre>
                      <pre data-prefix=">" className="text-[var(--cream)]"><code>your email:  </code>
                     <input name="email" type="email" value={visitorEmail} onChange={(e) => setVisitorEmail(e.target.value)}></input></pre>
                     <pre data-prefix=">" className="text-[var(--cream)]"><code>message:  </code>
                     <input name="message" type="text"value={visitorMessage} onChange={(e) => setVisitorMessage(e.target.value)}></input></pre>
-                    <pre className="flex justify-between my-5"><code><button id="contact-button" type="submit">sendMessage()</button></code></pre>
+                    <pre className="flex justify-between my-5"><code><button id="contact-button" type="submit" onClick={(e) => handleClick(e)}>sendMessage()</button></code></pre>
                 {submitMessage && 
                     <pre data-prefix="$" className="bg-success"><code>SUCCESS: message received </code></pre>
                 }
